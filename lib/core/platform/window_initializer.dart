47e18052463e2defffd1b66fa67476d23c6d8110
Ruhid Javadov <ruhidjavadoff@gmail.com>
2026-09-23 21:30:01 +0400

    fix(platform): maximize window on startup

--- lib/core/platform/window_initializer.dart
+++ lib/core/platform/window_initializer.dart
@@ -31,6 +31,7 @@ class WindowInitializer {
     await windowManager.waitUntilReadyToShow(options, () async {
       await windowManager.show();
       await windowManager.focus();
+      await windowManager.maximize();
     });
   }
 
